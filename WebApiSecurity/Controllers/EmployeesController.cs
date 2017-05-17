d22f0e2056f5313ffac3f716e33a36dd72eb3eca
정광일 <laitdei@imc.co.kr>
2017-05-18 00:58:49 +0900

    폼 로그인 처리

---
+++ WebApiSecurity/Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiSecurity.Controllers
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class EmployeesController : ApiController
+    {
+        public Employee Get(int id)
+        {
+            return new Employee
+            {
+                Id = id,
+                Name = "John Q Law",
+                Department = "Enforcement"
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return new Employee[]
+            {
+                new Employee
+                {
+                    Id = 12345,
+                    Name = "John Q Law",
+                    Department="Enforcement"
+                },
+                new Employee
+                {
+                    Id = 6789,
+                    Name = "John Q Taxpaper",
+                    Department = "Revenue"
+                }
+            };
+        }
+    }
+}
---
+++ WebApiSecurity/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApiSecurity.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string userId, string password)
+        {
+            if (userId.Equals(password))
+            {
+                FormsAuthentication.SetAuthCookie("Badri", false);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
